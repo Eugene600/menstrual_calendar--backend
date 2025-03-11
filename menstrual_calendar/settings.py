6c9c5c827176adf5c9c7d29bd2b04e35296a4fc4
Eugene600 <ugowach160@gmail.com>
2025-03-11 15:42:26 +0300

    Finalized managing of both backend and db containers using docker compose

--- menstrual_calendar/settings.py
+++ menstrual_calendar/settings.py
@@ -110,8 +110,8 @@ DATABASES = {
     'default': {
         'ENGINE': config('DATABASE_ENGINE'),
         'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
+        'USER': config('DATABASE_ROOT_USER'),
+        'PASSWORD': config('DATABASE_ROOT_PASSWORD'),
         'HOST': config('DATABASE_HOST'),
         'PORT': config('DATABASE_PORT'),
     }
